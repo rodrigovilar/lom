6753f174f50785a05da1b3c034bab06934c46b09
rodrigo <rodrigovilar@dce.ufpb.br>
2013-01-17 21:05:42 -0300

    Adding tests for Entity -- Valid behavior and one example of exception.
    perform tests
    
    exit
    
    perform tests
    
    exit
    
    perform tests
    
    exit

--- src/test/java/com/nanuvem/lom/model/EntityIntegrationTest.java
+++ src/test/java/com/nanuvem/lom/model/EntityIntegrationTest.java
@@ -1,12 +1,56 @@
 package com.nanuvem.lom.model;
 
+import javax.validation.ValidationException;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 
 @RooIntegrationTest(entity = Entity.class)
 public class EntityIntegrationTest {
 
+private Entity entity;
+
+	@Before
+	public void init() {
+    	entity = new Entity();		
+	}
+	
     @Test
-    public void testMarkerMethod() {
+    public void validNameAndNamespace() {
+    	entity.setName("name");
+    	entity.setNamespace("namespace");
+		entityService.saveEntity(entity);
+    }
+    
+    @Test
+    public void withoutNamespace() {
+    	entity.setName("name");
+		entityService.saveEntity(entity);
+    }
+    
+    @Test
+    public void twoEntitiesSameNameDifferentNamespaces() {
+    	entity.setName("sameName");
+    	entity.setNamespace("namespace1");
+		entityService.saveEntity(entity);
+		
+		Entity entity2 = new Entity();
+    	entity2.setName("sameName");
+    	entity2.setNamespace("namespace2");
+		entityService.saveEntity(entity2);    	
+    }
+
+    @Test
+    public void nameAndNamespaceWithSpaces() {
+    	entity.setName("name with spaces");
+    	entity.setNamespace("namespace with spaces");
+		entityService.saveEntity(entity);
+    }
+
+    @Test(expected=ValidationException.class)
+    public void withoutName() {
+    	entity.setNamespace("has only namespace");
+		entityService.saveEntity(entity);
     }
 }
