e92493f1a5d929b52b130a348ac7ee32abb08906
rodrigo <rodrigovilar@dce.ufpb.br>
2013-04-05 15:56:15 -0300

    Adding Service Layer for Instance, Property and PropertyValue

---
+++ engine/src/main/java/com/nanuvem/lom/service/PropertyValueService.java
@@ -0,0 +1,7 @@
+package com.nanuvem.lom.service;
+
+import org.springframework.roo.addon.layers.service.RooService;
+
+@RooService(domainTypes = { com.nanuvem.lom.model.PropertyValue.class })
+public interface PropertyValueService {
+}
