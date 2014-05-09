afb4eb77c14666086f4e41002a8c52186228e2ef
Fernando M. Oliveira <fernando.dsfw@gmail.com>
2014-05-09 02:06:06 -0300

    Test cases: 'unknownClass', 'nullClass' and 'classWithKnownAttributesAndWithoutConfiguration' for Instance. Adding the validation AttributeValue in InstanceHelper

--- kernel/src/test/java/com/nanuvem/lom/kernel/InstanceServiceTest.java
+++ kernel/src/test/java/com/nanuvem/lom/kernel/InstanceServiceTest.java
@@ -8,15 +8,33 @@ import com.nanuvem.lom.kernel.dao.memory.MemoryDaoFactory;
 public class InstanceServiceTest {
 
 	private ClassServiceImpl classService;
-	private AttributeServiceImpl attributeService;
+	private AttributeServiceImpl attributeServiceImpl;
 	private InstanceServiceImpl instanceService;
 
 	@Before
 	public void init() {
 		MemoryDaoFactory daoFactory = new MemoryDaoFactory();
 		this.classService = new ClassServiceImpl(daoFactory);
-		this.attributeService = new AttributeServiceImpl(daoFactory);
+		this.attributeServiceImpl = new AttributeServiceImpl(daoFactory);
 		this.instanceService = new InstanceServiceImpl(daoFactory);
+	}
+
+	@Test
+	public void unknownClass() {
+		InstanceHelper.expectExceptionOnCreateInvalidInstance(instanceService,
+				"default.a", "Unknown class: default.a",
+				attributeValue("age", 30));
+
+		InstanceHelper.expectExceptionOnCreateInvalidInstance(instanceService,
+				"abc.a", "Unknown class: abc.a", attributeValue("age", 30));
+	}
+
+	@Test
+	public void nullClass() {
+		InstanceHelper.expectExceptionOnCreateInvalidInstance(instanceService,
+				null,
+				"Invalid value for Instance class: The class is mandatory",
+				attributeValue("age", 30));
 	}
 
 	@Test
@@ -41,6 +59,60 @@ public class InstanceServiceTest {
 		InstanceHelper.expectExceptionOnCreateInvalidInstance(instanceService,
 				"system.Client", "Unknown attribute for system.Client: age",
 				attributeValue("age", 30));
+
+		InstanceHelper.expectExceptionOnCreateInvalidInstance(instanceService,
+				"system.Client", "Unknown attribute for system.Client: height",
+				attributeValue("height", 1.85));
+
+		InstanceHelper.expectExceptionOnCreateInvalidInstance(instanceService,
+				"system.Client", "Unknown attribute for system.Client: name",
+				attributeValue("name", "John"));
+
+		InstanceHelper.expectExceptionOnCreateInvalidInstance(instanceService,
+				"system.Client", "Unknown attribute for system.Client: active",
+				attributeValue("active", true));
+	}
+
+	@Test
+	public void classWithKnownAttributesAndWithoutConfiguration() {
+		ClassHelper.createClass(classService, "system", "Client");
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.Client", null, "pa", AttributeType.TEXT, null);
+
+		ClassHelper.createClass(classService, "system", "User");
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.User", null, "pa", AttributeType.TEXT, null);
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.User", null, "pb", AttributeType.LONGTEXT, null);
+
+		ClassHelper.createClass(classService, "system", "Organization");
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.Organization", null, "pa", AttributeType.TEXT, null);
+		AttributeHelper
+				.createOneAttribute(this.attributeServiceImpl,
+						"system.Organization", null, "pb",
+						AttributeType.LONGTEXT, null);
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.Organization", null, "pc", AttributeType.INTEGER, null);
+
+		ClassHelper.createClass(classService, "system", "Category");
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.Category", null, "pa", AttributeType.TEXT, null);
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.Category", null, "pb", AttributeType.LONGTEXT, null);
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.Category", null, "pc", AttributeType.INTEGER, null);
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl,
+				"system.Category", null, "pd", AttributeType.PASSWORD, null);
+
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"system.Client");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"system.User");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"system.Organization");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"system.Category");
 	}
 
 	private static AttributeValue attributeValue(String attributeName,
