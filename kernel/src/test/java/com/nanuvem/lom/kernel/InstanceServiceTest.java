19363319ee3bced93b662fcd4ae23c9f2fad75cb
Fernando M. Oliveira <fernando.dsfw@gmail.com>
2014-05-27 15:05:52 -0300

    creation of test instanceWithValidValuesForTheConfigurationOfAttributesText

--- kernel/src/test/java/com/nanuvem/lom/kernel/InstanceServiceTest.java
+++ kernel/src/test/java/com/nanuvem/lom/kernel/InstanceServiceTest.java
@@ -114,6 +114,140 @@ public class InstanceServiceTest {
 				"system.Category");
 	}
 
+	@Test
+	public void instanceWithValidValuesForTheConfigurationOfAttributesText() {
+		ClassHelper.createClass(this.classService, "abc", "a");
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl, "abc.a",
+				null, "name", AttributeType.TEXT, "{\"mandatory\": true}");
+		AttributeValue value1 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.a", "Jose");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"system.Client", value1);
+
+		ClassHelper.createClass(this.classService, "abc", "b");
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl, "abc.b",
+				null, "name", AttributeType.TEXT,
+				"{\"mandatory\": true, \"default\": \"Michael\"}");
+		AttributeValue value2 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.b", null);
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.b", value2);
+
+		ClassHelper.createClass(classService, "abc", "c");
+		AttributeHelper
+				.createOneAttribute(this.attributeServiceImpl, "abc.c", null,
+						"name", AttributeType.TEXT,
+						"{\"mandatory\": true, \"default\": \"Johson\", \"minlength\": \"6\"}");
+		AttributeValue value3 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.c", null);
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.c", value3);
+
+		ClassHelper.createClass(classService, "abc", "d");
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl, "abc.d",
+				null, "name", AttributeType.TEXT,
+				"{\"mandatory\": true, \"minlength\": \"6\"}");
+		AttributeValue value4 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.d", "Johson");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.d", value4);
+
+		ClassHelper.createClass(classService, "abc", "e");
+		AttributeHelper
+				.createOneAttribute(this.attributeServiceImpl, "abc.e", null,
+						"name", AttributeType.TEXT,
+						"{\"mandatory\": true, \"default\": \"Johson\", \"maxlength\" : 6}");
+		AttributeValue value5 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.e", null);
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.e", value5);
+
+		ClassHelper.createClass(classService, "abc", "f");
+		AttributeHelper.createOneAttribute(this.attributeServiceImpl, "abc.f",
+				null, "name", AttributeType.TEXT,
+				"{\"mandatory\": true, \"maxlength\" : 6}");
+		AttributeValue value6 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.f", "Johnson");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.f", value6);
+
+		ClassHelper.createClass(classService, "abc", "g");
+		AttributeHelper
+				.createOneAttribute(
+						this.attributeServiceImpl,
+						"abc.g",
+						null,
+						"name",
+						AttributeType.TEXT,
+						"{\"mandatory\": true, \"default\": \"Johson\", \"minlength\": \"6\", \"maxlength\" : 6}");
+		AttributeValue value7 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.g", null);
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.g", value7);
+
+		ClassHelper.createClass(classService, "abc", "i");
+		AttributeHelper
+				.createOneAttribute(this.attributeServiceImpl, "abc.i", null,
+						"name", AttributeType.TEXT,
+						"{\"mandatory\": true, \"minlength\": \"6\", \"maxlength\" : 6}");
+		AttributeValue value8 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.i", "Johson");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.i", value8);
+
+		ClassHelper.createClass(classService, "abc", "j");
+		AttributeHelper
+				.createOneAttribute(
+						this.attributeServiceImpl,
+						"abc.j",
+						null,
+						"name",
+						AttributeType.TEXT,
+						"{\"mandatory\": true, \"default\": \"Johson\", \"minlength\": \"3\", \"maxlength\" : 8}");
+		AttributeValue value9 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.j", null);
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.j", value9);
+
+		ClassHelper.createClass(classService, "abc", "k");
+		AttributeHelper
+				.createOneAttribute(this.attributeServiceImpl, "abc.k", null,
+						"name", AttributeType.TEXT,
+						"{\"mandatory\": true, \"minlength\": \"3\", \"maxlength\" : 8}");
+		AttributeValue value10 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.k", "Johnson");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.k", value10);
+
+		ClassHelper.createClass(classService, "abc", "l");
+		AttributeHelper
+				.createOneAttribute(
+						this.attributeServiceImpl,
+						"abc.l",
+						null,
+						"name",
+						AttributeType.TEXT,
+						"{\"mandatory\": true, \"default\": \"abc@abc.com\", \"minlength\": \"3\", \"maxlength\" : 15, \"regex\": \"(\\\\w[-.\\\\w]\\\\w@\\\\w[-._\\\\w]\\\\w.\\\\w{2,3})\"}");
+		AttributeValue value11 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.l", null);
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.l", value11);
+
+		ClassHelper.createClass(classService, "abc", "m");
+		AttributeHelper
+				.createOneAttribute(
+						this.attributeServiceImpl,
+						"abc.m",
+						null,
+						"name",
+						AttributeType.TEXT,
+						"{\"mandatory\": true, \"minlength\": \"3\", \"maxlength\" : 15, \"regex\": \"(\\\\w[-.\\\\w]\\\\w@\\\\w[-._\\\\w]\\\\w.\\\\w{2,3})\"}");
+		AttributeValue value12 = InstanceHelper.createOneAttributeValue(
+				this.attributeServiceImpl, "name", "abc.m", "abc@abc.com");
+		InstanceHelper.createAndVerifyOneInstance(this.instanceService,
+				"abc.m", value12);
+	}
+
 	private static AttributeValue attributeValue(String attributeName,
 			Object objValue) {
 		Attribute attribute = new Attribute();
